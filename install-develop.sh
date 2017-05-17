1c3be2798c206a2a43d1164d844ba8f0c2a74bab
fritz-smh <fritz.smh@gmail.com>
2017-05-17 15:36:06 +0200

    new build

--- install-develop.sh
+++ install-develop.sh
@@ -647,8 +647,7 @@ title "Install the dependencies"
 ########################################
 #  Install dependencies for Debian 8.6 #
 ########################################
-
-    # All the lines are prefixed by '   ' to get a final script more clear to read
+# All the lines are prefixed by '   ' to get a final script more clear to read
 
     ### Check if this is a Debian release
     OS="unknown"
@@ -668,7 +667,8 @@ title "Install the dependencies"
         ### Update the packages list
         apt-get update
         
-        ### LSB release
+        ### Install packages
+        # LSB release
         #Domogik installation script uses the ``lsb_release -si`` command to check which Linux distribution you are using. Some Linux distribution has not this package instlled by default. This is the case for **Raspbian** for example.
         #
         #On all Debian-based distributions (Raspbian for example), we install the **lsb-release** package
@@ -679,62 +679,27 @@ title "Install the dependencies"
         apt-get -y install python2.7
         apt-get -y install python2.7-dev python-pip
 
-        # TODO : DEL
-        # TODO : DEL
-        # TODO : DEL
-        #pip install netifaces
-        #pip install sphinx-better-theme
-
-        ### Remove python-cffi
+        # Remove python-cffi
         # python-cffi is installed with the previous command (apt-get -y install python2.7-dev python-pip)...
         #
         # This is needed because the installed release is too old (8.6.1) and used by python instead of the one installed with pip
         # which is needed to avoid some setuptools_ext import error.
         apt-get -y remove python-cffi
 
-        ### OpenSSL dev files
+        # Various dependencies
         apt-get -y install libssl-dev
-
-        ### Zlib dev files
         apt-get -y install zlib1g-dev
-
-        ### Libffi-dev
         apt-get -y install libffi-dev
         
-        ### Specific about Debian stable (8.6)
-        # If you are using a Debian stable, you will need to install a more recent release of **alembic** related package. You will have to follow these steps.
-        #
-        #Create the file **/etc/apt/apt.conf.d/99defaultrelease**. It must contain : ::
-        #
-        #    APT::Default-Release "stable";
-        #
-        #Create the file **/etc/apt/sources.list.d/stable.list** : ::
-        #
-        #    deb     http://ftp.fr.debian.org/debian/    stable main contrib non-free
-        #    deb-src http://ftp.fr.debian.org/debian/    stable main contrib non-free
-        #    deb     http://security.debian.org/         stable/updates  main contrib non-free
-        #
-        #Create the file **/etc/apt/sources.list.d/testing.list** : ::
-        #
-        #    deb     http://ftp.fr.debian.org/debian/    testing main contrib non-free
-        #    deb-src http://ftp.fr.debian.org/debian/    testing main contrib non-free
-        #    deb     http://security.debian.org/         testing/updates  main contrib non-free
-        #
-        #Then run : ::
-        #
-        #    $ sudo apt-get update
-        #    $ sudo apt-get -t testing install python-sqlalchemy python-editor python-sqlalchemy python-alembic
-        #
-        #It will install the needed packages from the testing repository.
-        
-        
-    
         ### MySQL/MariaDB server
         
         # in case, this is not already installed, we automatically set a root password during installation
         export DEBIAN_FRONTEND=noninteractive
 
-        echo "PASSWORD=$MYSQL_ROOT_PASSWORD"
+        # For debug, if needed :
+        #echo "PASSWORD=$MYSQL_ROOT_PASSWORD"
+
+        # Non working part : the password is not used, but this way, no password is set so this is quite usefull during the install!
         #debconf-set-selections <<< "mariadb-server/root_password password $MYSQL_ROOT_PASSWORD"
         #debconf-set-selections <<< "mariadb-server/root_password_again password $MYSQL_ROOT_PASSWORD"
         # TODO : handle this : 
@@ -745,18 +710,11 @@ title "Install the dependencies"
         # debconf: (This frontend requires a controlling tty.)
         # debconf: falling back to frontend: Teletype
         # dpkg-preconfigure: unable to re-open stdin: 
-
-    
     
         apt-get -y install mariadb-server
-        # TODO : how to not prompt the user for a mysql admin password on install ?
-        # TODO : how to not prompt the user for a mysql admin password on install ?
-        # TODO : how to not prompt the user for a mysql admin password on install ?
-        # TODO : how to not prompt the user for a mysql admin password on install ?
         
     else 
         echo "Not a Debian"
-        exit 1
     fi
 
 
